ecd0958277f6638f7d4189939b943a15022da051
sky <sky.xctc@163.com>
2011-12-16 16:46:55 +0800

    first init and init user login gui

---
+++ src/com/iped/ipcam/gui/WebCam.java
@@ -0,0 +1,14 @@
+package com.iped.ipcam.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WebCam extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+    }
+}
