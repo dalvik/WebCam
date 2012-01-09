6a2a2002a0344f41e749dd6d4efbb4fe2658d5a5
sky <sky.xctc@163.com>
2012-01-09 15:21:55 +0800

    move MyVideoView out of

---
+++ src/com/iped/ipcam/gui/MyVideoView.java
@@ -0,0 +1,213 @@
+package com.iped.ipcam.gui;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Bitmap.Config;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.View;
+
+import com.iped.ipcam.utils.CamCmdListHelper;
+import com.iped.ipcam.utils.Constants;
+
+public class MyVideoView extends View implements Runnable {
+
+	private final static int NALBUFLENGTH = 320*480 * 2;
+	
+	private final static int SOCKETBUFLENGTH = 342000;
+	
+	private Bitmap video = Bitmap.createBitmap(320, 480, Config.RGB_565);
+	
+	byte[] pixel = new byte[NALBUFLENGTH];
+	
+	byte[] nalBuf = new byte[NALBUFLENGTH];// 80k
+
+	ByteBuffer buffer = ByteBuffer.wrap(nalBuf);
+	
+	int nalSizeTemp = 0;
+	
+	int nalBufUsedLength = 0;
+	
+	byte[] socketBuf = new byte[SOCKETBUFLENGTH];
+	
+	FileInputStream fis = null;
+	
+	int readLengthFromSocket = 0;
+	
+	int sockBufferUsedLength;
+	
+	boolean firstStartFlag = true;
+	
+	boolean looperFlag = false;
+	
+	private Socket socket = null;
+	
+	private DataInputStream dis = null;
+	
+	private boolean stopPlay = false;
+
+	private static final String TAG = "ReadStreamThread";
+	
+	private Handler handler;
+	
+	public MyVideoView(Context context) {
+		super(context);
+	}
+	
+	public MyVideoView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	void init(Handler handler) {
+		this.handler = handler;
+	}
+	
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		if(video != null) {
+			canvas.drawBitmap(video, 0, 0, null);
+		}
+	}
+	
+	public void run() {
+		try {
+			byte [] tem = CamCmdListHelper.SetCmd_StartVideo.getBytes();
+			DatagramSocket datagramSocket = new DatagramSocket();
+			datagramSocket.setSoTimeout(Constants.VIDEOSEARCHTIMEOUT);
+			//datagramPacket = new DatagramPacket(tem, tem.length, InetAddress.getByName(Constants.DEFAULTSEARCHIP + i), Constants.UDPPORT);
+			DatagramPacket datagramPacket = new DatagramPacket(tem, tem.length, InetAddress.getByName("192.168.1.141"), 60000);
+			datagramSocket.send(datagramPacket);
+			System.out.println("send udp packet...");
+			//DatagramPacket rece = new DatagramPacket(buffTemp, buffTemp.length);
+			System.out.println("ready rece ....");
+			//datagramSocket.receive(rece);
+			//String info = new String(buffTemp);
+			//if(info != null) {
+				//Log.d(TAG, "receive inof = : " + info);
+				SocketAddress socketAddress = new InetSocketAddress("192.168.1.141", 1234);
+				socket = new Socket();
+				socket.connect(socketAddress, 15000);
+				dis = new DataInputStream(socket.getInputStream());
+				System.out.println("dis=" + dis);
+			//}
+			//fis = new FileInputStream(new File("/sdcard/video_test.dat"));
+			//fis = new FileInputStream(new File("/sdcard/a.jpeg"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		long start = System.currentTimeMillis();
+		int i = 0;
+		while (!Thread.currentThread().isInterrupted() && dis != null && !stopPlay) {
+			try {
+				readLengthFromSocket = dis.read(socketBuf,0, SOCKETBUFLENGTH);//   从文件流里面读取的字节的长度  <0时读取完毕
+			} catch (IOException e) {
+				Log.d(TAG, e.getLocalizedMessage());
+			}
+			if (readLengthFromSocket <= 0) { // 读取完成
+				System.out.println("read over break....");
+				break;
+			}
+			sockBufferUsedLength = 0;
+			while(readLengthFromSocket - sockBufferUsedLength>0) {// remain socket buf length
+				try {
+					nalSizeTemp = mergeBuffer(nalBuf, nalBufUsedLength, socketBuf, sockBufferUsedLength, (readLengthFromSocket - sockBufferUsedLength));
+				}catch(Exception e) {
+					
+				}
+				while(looperFlag) {
+					looperFlag = false;
+					if(nalSizeTemp == -2) {
+						if(nalBufUsedLength>0) {
+							i++;
+							if(i%50 ==0) {
+								long end = System.currentTimeMillis() / 1000 - start / 1000;
+								System.out.println("pic index=" + i +" use time" + end +  " rate:" + i/(end)+ " p/s");
+							}
+							copyPixl();
+						}
+						nalBuf[0] = -1;
+						nalBuf[1] = -40;
+						nalBuf[2] = -1;
+						nalBuf[3] = -32;
+						sockBufferUsedLength += 4;
+						nalBufUsedLength = 4;
+						break;
+					}
+				}
+			}
+		}
+		System.out.println("onstop===="  + stopPlay);
+		if(fis != null) {
+			try {
+				fis.close();
+				Log.d("CamVideoH264", "over......."  + Thread.currentThread().isInterrupted());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void copyPixl() {
+		video = BitmapFactory.decodeByteArray(nalBuf, 0, nalBufUsedLength);
+		if(video != null) {
+			postInvalidate();
+		}
+	}
+	
+	 
+	private int mergeBuffer(byte[] nalBuf, int nalBufUsed, byte[] socketBuf, int sockBufferUsed, int socketBufRemain) {
+		int i = 0;
+		for(i=0; i<socketBufRemain; i++) {
+			if(firstStartFlag && socketBuf[i] == 0 && socketBuf[i+1] == 0 && socketBuf[i+2] == 0 && socketBuf[i+3] == 1) {
+				firstStartFlag = false;
+				sockBufferUsedLength += 65;
+				looperFlag = true;
+				return -1;
+			} else if(socketBuf[i + sockBufferUsed] == -1 && socketBuf[i + 1 + sockBufferUsed] == -40 && socketBuf[i + 2 + sockBufferUsed] == -1 && socketBuf[i + 3+ sockBufferUsed] == -32) {
+				/*if((i + 3+ sockBufferUsed) < SOCKETBUFLENGTH) {
+					if(socketBuf[i + sockBufferUsed] == -1 && socketBuf[i + 1 + sockBufferUsed] == -40 && socketBuf[i + 2 + sockBufferUsed] == -1 && socketBuf[i + 3+ sockBufferUsed] == -32) {
+						looperFlag = true;
+						return -2;
+					} 
+				} else {
+					nalBuf[i+nalBufUsed] = socketBuf[i + sockBufferUsed];
+					nalBufUsedLength++;
+					sockBufferUsedLength++;
+				}*/
+				looperFlag = true;
+				return -2;
+			}else {
+				nalBuf[i+nalBufUsed] = socketBuf[i + sockBufferUsed];
+				nalBufUsedLength++;
+				sockBufferUsedLength++;
+			}
+		}
+		looperFlag = true;
+		return i;
+	}
+	
+	public void onStop() {
+		stopPlay = true;
+	}
+
+}
