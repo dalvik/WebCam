5121db476f7dd1f91136ca85abe1cdad1a831b83
sky <sky.xctc@163.com>
2012-01-16 10:35:16 +0800

    add file or dir explorer2

---
+++ src/com/iped/ipcam/gui/DirPreview.java
@@ -0,0 +1,150 @@
+package com.iped.ipcam.gui;
+
+import java.io.File;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+
+import com.iped.ipcam.utils.FilePathAdapter;
+import com.iped.ipcam.utils.FileUtil;
+
+public class DirPreview extends ListActivity implements OnClickListener {
+
+	private ListView listView = null;
+	
+	private List<String> filePathList = null;
+	
+	private FilePathAdapter adapter = null;
+	
+	private String rootPath = FileUtil.getDefaultPath();
+	
+	private String current = rootPath;
+	
+	private TextView titlePath = null;
+	
+	private Button backButton = null;
+	
+	private Button okButton = null;
+	
+	private Button cancleButton = null;
+	
+	private Button choceButton = null;
+	
+	private boolean flag = true;
+
+	private OnItemClickListener itemClickListener = new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(AdapterView<?> arg0, View arg1, int index,
+				long arg3) {
+			String temPath = adapter.getItem(index);
+			viewFiles(temPath);
+			listView.requestFocusFromTouch();
+			listView.setSelection(index);
+		}
+	};
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.books_list);
+		titlePath = (TextView) findViewById(R.id.path_title);
+		backButton = (Button) findViewById(R.id.dir_preview_back_button);
+		okButton = (Button) findViewById(R.id.system_settings_save_path_preview_sure_id);
+		cancleButton = (Button) findViewById(R.id.system_settings_save_path_preview_cancle_id);
+		choceButton = (Button) findViewById(android.R.id.empty);
+		backButton.setOnClickListener(this);
+		okButton.setOnClickListener(this);
+		cancleButton.setOnClickListener(this);
+		choceButton.setOnClickListener(this);
+		titlePath.setText(rootPath);
+		Intent intent = getIntent();
+		if(intent != null) {
+			flag = intent.getBooleanExtra("DIRPREVIEW", true);
+		} else {
+			flag = true;
+		}
+		filePathList = FileUtil.getFiles(DirPreview.this, rootPath, flag);
+		adapter = new FilePathAdapter(this, filePathList);
+		listView = getListView();
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(itemClickListener);
+	}
+	
+	public void viewFiles(String filePath) {
+		List<String> temp = FileUtil.getFiles(DirPreview.this, filePath, flag);
+		if(temp != null) {
+			current = filePath;
+			titlePath.setText(current);
+			filePathList.clear();
+			filePathList.addAll(temp);
+			adapter.notifyDataSetChanged();
+		}
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if(keyCode == KeyEvent.KEYCODE_BACK) {
+			cancle();
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+	
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.dir_preview_back_button:
+			File file = new File(current);
+			String parentPath = file.getParent();
+			if(parentPath != null) {
+				viewFiles(parentPath);
+			}
+			break;
+		case R.id.system_settings_save_path_preview_sure_id:
+			ok();
+			break;
+		case R.id.system_settings_save_path_preview_cancle_id:
+			cancle();
+			break;
+		case android.R.id.empty:
+			ok();
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void ok() {
+		Intent intent = new Intent();
+		if(flag) {
+			intent.putExtra("DIRFILEPATH", titlePath.getText().toString());
+			setResult(100, intent);
+		} else {
+			String path = titlePath.getText().toString();
+			File file = new File(path);
+			if(!file.isFile()) {
+				return;
+			}
+			intent.putExtra("BINFILEPATH", titlePath.getText().toString());
+			setResult(200, intent);
+		}
+		DirPreview.this.finish();
+	}
+	
+	private void cancle() {
+		setResult(RESULT_CANCELED, new Intent());
+		DirPreview.this.finish();
+	}
+
+}
--- src/com/iped/ipcam/gui/SystemSettings.java
+++ src/com/iped/ipcam/gui/SystemSettings.java
@@ -1,13 +1,134 @@
 package com.iped.ipcam.gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SystemSettings extends Activity {
+import com.iped.ipcam.engine.CamMagFactory;
+import com.iped.ipcam.engine.ICamManager;
+import com.iped.ipcam.pojo.Device;
+import com.iped.ipcam.utils.Constants;
+import com.iped.ipcam.utils.FileUtil;
 
+public class SystemSettings extends Activity implements android.view.View.OnClickListener{
+
+	private List<String> files = new ArrayList<String>();
+	
+	private ArrayAdapter<String> array = null; 
+		
+	private EditText videoSavePath = null;
+	
+	private EditText deviceName = null;
+	
+	private EditText devicdIP = null;
+	
+	private SharedPreferences settings = null;
+	
+	private Button dirPreviewButton = null;
+	
+	private EditText binPathEditText = null;
+	
+	private Button binDirPreviewButton = null;
+	
+	private Button buttonUpdate = null;
+	
+	private String rootPath = FileUtil.getDefaultPath();
+	
+	private AlertDialog alg = null;
+	
+	private Handler handler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			Intent intent = new Intent(SystemSettings.this, DirPreview.class);
+			switch (msg.what) {
+			case Constants.SHOWPREVIEWDIRDLG:
+				//showDirPreviewDlg(R.string.system_settings_save_path_preview_str,true);
+				intent.putExtra("DIRPREVIEW", true);
+				startActivityForResult(intent, 100);
+				break;
+			case Constants.SHOWBINPREVIEWDIRDLG:
+				//showDirPreviewDlg(R.string.system_settings_save_path_preview_str,true);
+				intent.putExtra("DIRPREVIEW", false);
+				startActivityForResult(intent, 200);
+				break;
+			default:
+				break;
+			}
+		}
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.system_settings);
+		videoSavePath = (EditText) findViewById(R.id.system_settings_save_path_id);
+		deviceName = (EditText) findViewById(R.id.system_settings_device_name_id);
+		devicdIP = (EditText) findViewById(R.id.system_settings_device_ip_id);
+		dirPreviewButton = (Button) findViewById(R.id.system_settings_preview_id);
+		binPathEditText = (EditText) findViewById(R.id.system_settings_device_bin_addr_id);
+		binDirPreviewButton = (Button) findViewById(R.id.system_settings_device_bin_update_id);
+		buttonUpdate = (Button) findViewById(R.id.system_settings_device_update_id);
+		
+		dirPreviewButton.setOnClickListener(this);
+		binDirPreviewButton.setOnClickListener(this);
+		buttonUpdate.setOnClickListener(this);
+		settings = getPreferences(0);
+		String defaultPath = settings.getString("VIDEOSAVEPATH", "D:\\Program Files\\IP CAM ¼à¿ØÈí¼þ\\");
+		videoSavePath.setText(defaultPath);
+		ICamManager camManager = CamMagFactory.getCamManagerInstance();
+		Device device = camManager.getSelectDevice();
+		if(device != null) {
+			deviceName.setText(device.getDeviceName());
+			devicdIP.setText(device.getDeviceIp());
+		}
 	}
+
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.system_settings_preview_id:
+			handler.sendEmptyMessage(Constants.SHOWPREVIEWDIRDLG);
+			break;
+		case R.id.system_settings_device_bin_update_id:
+			handler.sendEmptyMessage(Constants.SHOWBINPREVIEWDIRDLG);
+			break;
+		case R.id.system_settings_device_update_id:
+			
+			break;
+		default:
+			break;
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if(resultCode == 100) {
+			String savePath = data.getStringExtra("DIRFILEPATH");
+			System.out.println(requestCode + " " + resultCode + " " + data.getStringExtra("FILEPATH"));
+			if(savePath != null) {
+				videoSavePath.setText(savePath);
+				settings.edit().putString("VIDEOSAVEPATH", savePath).commit();
+			}
+		} else if(resultCode == 200) {
+			String binPath = data.getStringExtra("BINFILEPATH");
+			if(binPath != null) {
+				binPathEditText.setText(binPath);
+			}
+			System.out.println(requestCode + " " + resultCode + " " + data.getStringExtra("BINFILEPATH"));
+		}
+	}
+	
 }
