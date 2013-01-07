841437fc1a214e4272f4e05bcd2e158b52c79ff9
dalvik <sky.xctc@163.com>
2013-01-07 13:02:23 +0800

    add play sound in a single thread but not success

--- src/com/iped/ipcam/engine/DecodeAudioThread.java
+++ src/com/iped/ipcam/engine/DecodeAudioThread.java
@@ -1,21 +1,24 @@
 package com.iped.ipcam.engine;
 
+import android.graphics.BitmapFactory;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+
 import com.iped.ipcam.engine.PlayMpegThread.OnMpegPlayListener;
 import com.iped.ipcam.factory.DecoderFactory;
 import com.iped.ipcam.gui.BuildConfig;
 import com.iped.ipcam.gui.MyVideoView;
-import com.iped.ipcam.gui.MyVideoView.OnPutIndexListener;
 import com.iped.ipcam.gui.UdtTools;
+import com.iped.ipcam.pojo.JpegImage;
+import com.iped.ipcam.utils.ByteUtil;
 import com.iped.ipcam.utils.Command;
+import com.iped.ipcam.utils.PlayBackConstants;
 
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
-import android.util.Log;
-
-public class DecodeAudioThread extends DecoderFactory implements OnPutIndexListener{
-
-	private AudioTrack m_out_trk = null;
+public class DecodeAudioThread extends DecoderFactory{
 
 	private String TAG = "DecodeAudioThread";
 	
@@ -31,38 +34,28 @@ public class DecodeAudioThread extends DecoderFactory implements OnPutIndexListe
 	
 	private byte[] pcmArr = new byte[pcmBufferLength];
 	
+	private byte[] playAudioBuf = new byte[RECEAUDIOBUFFERSIZE];
+	
 	private int zeroIndex32 = 0;
 	
+	private int recvAudioBufLen = 50 * 1024;
+	private byte[] recvAudioBuf = null; 
+	private int indexForPut = 0; // put索引 （下一个要写入的位置）
+	private int indexForGet = 0;
 	
 	public DecodeAudioThread(MyVideoView myVideoView) {
-		myVideoView.setOnPutIndexListener(this);
+		recvAudioBuf = new byte[recvAudioBufLen];
 	}
 	
 	
 	@Override
 	public void run() {
-		int init = UdtTools.initAmrDecoder();
-		if(BuildConfig.DEBUG && DEBUG) {
-			Log.d(TAG, "amr deocder init " + init);
-		}
-		if (m_out_trk != null) {
-			m_out_trk.stop();
-			m_out_trk.release();
-			m_out_trk = null;
-		}
-		
-		int m_out_buf_size = android.media.AudioTrack.getMinBufferSize(
-				8000, AudioFormat.CHANNEL_CONFIGURATION_MONO,
-				AudioFormat.ENCODING_PCM_16BIT);
-		m_out_trk = new AudioTrack(AudioManager.STREAM_MUSIC, 8000,
-				AudioFormat.CHANNEL_CONFIGURATION_MONO,
-				AudioFormat.ENCODING_PCM_16BIT, m_out_buf_size * 10,
-				AudioTrack.MODE_STREAM);
-		m_out_trk.play();
-		
+		new Thread(new PlaySoundThread()).start();
 		int recvDataLength = -1;
 		while (!stopPlay) {
+			Log.d(TAG, "### start ");
 			recvDataLength = UdtTools.recvAudioMsg(RECEAUDIOBUFFERSIZE, audioBuffer, RECEAUDIOBUFFERSIZE);
+			Log.d(TAG, "### audio = " + recvDataLength);
 			if(recvDataLength<=0) {
 				if(BuildConfig.DEBUG && DEBUG) {
 					Log.d(TAG, "### audio recv audio over");
@@ -70,30 +63,23 @@ public class DecodeAudioThread extends DecoderFactory implements OnPutIndexListe
 				stopPlay = true;
 				break;
 			}
-			zeroIndex32 = 0;
-			for(int i = 0; i<recvDataLength; i++) {// 判断是否含有32个零
-				if(audioBuffer[i]==0) {
-					zeroIndex32++;
-					if(zeroIndex32 == 32) {
-						stopPlay = true;
-						break;
+			int recvBufIndex = 0;
+			do{
+				if((indexForPut +1) % recvAudioBufLen == indexForGet) {
+					synchronized (recvAudioBuf) {
+						try {
+							recvAudioBuf.wait(10);
+							Log.d(TAG, "### audio buffer is full! ---->");
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
 					}
 				}else {
-					zeroIndex32 = 0;
+					recvAudioBuf[indexForPut]= audioBuffer[recvBufIndex];  
+					indexForPut = (indexForPut+1) % recvAudioBufLen;  
+				    recvBufIndex++;
 				}
-			}
-			UdtTools.amrDecoder(audioBuffer, recvDataLength, pcmArr, 0, Command.CHANEL);
-			m_out_trk.write(pcmArr, 0, pcmBufferLength);
-		}
-		
-		if (m_out_trk != null) {
-			m_out_trk.stop();
-			m_out_trk.release();
-			m_out_trk = null;
-		}
-		UdtTools.exitAmrDecoder();
-		if(BuildConfig.DEBUG && DEBUG) {
-			Log.d(TAG, "### audio amr decoder exit.");
+			}while(recvDataLength>recvBufIndex && !stopPlay);
 		}
 	}
 
@@ -104,19 +90,94 @@ public class DecodeAudioThread extends DecoderFactory implements OnPutIndexListe
 
 
 	@Override
-	public void updatePutIndex(int putIndex) {
-		
-	}
-	
-	@Override
 	public int getIndexForGet() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 	
 	@Override
 	public void setOnMpegPlayListener(OnMpegPlayListener listener) {
-		// TODO Auto-generated method stub
+	}
+	
+	private class PlaySoundThread implements Runnable {
+
+		private AudioTrack m_out_trk = null;
+		
+		private int playAudioBufIndex = 0;
+		
+		public PlaySoundThread() {
+			int init = UdtTools.initAmrDecoder();
+			if(BuildConfig.DEBUG && DEBUG) {
+				Log.d(TAG, "amr deocder init " + init);
+			}
+		}
+		
+		@Override
+		public void run() {
+			if (m_out_trk != null) {
+				m_out_trk.stop();
+				m_out_trk.release();
+				m_out_trk = null;
+			}
+			int m_out_buf_size = android.media.AudioTrack.getMinBufferSize(
+					8000, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+					AudioFormat.ENCODING_PCM_16BIT);
+			m_out_trk = new AudioTrack(AudioManager.STREAM_MUSIC, 8000,
+					AudioFormat.CHANNEL_CONFIGURATION_MONO,
+					AudioFormat.ENCODING_PCM_16BIT, m_out_buf_size * 10,
+					AudioTrack.MODE_STREAM);
+			m_out_trk.play();
+			zeroIndex32 = 0;
+			do{
+				if((indexForGet+1) % recvAudioBufLen == indexForPut){
+					synchronized (pcmArr) {
+						if(BuildConfig.DEBUG && DEBUG) {
+							Log.d(TAG, "### audio buffer is empty! ---->");
+						}
+						try {
+							pcmArr.wait(50);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}  
+				}else {
+					byte b0 = recvAudioBuf[indexForGet];
+					if(b0 == 0) {
+						zeroIndex32++;
+						if(zeroIndex32 == 32) {
+							stopPlay = true;
+							Log.d(TAG, "### 0000000000000000000000000000000");
+							break;
+						}
+					}else {
+						zeroIndex32 = 0;
+					}
+					if(++playAudioBufIndex % (RECEAUDIOBUFFERSIZE) == 0) {
+						//Log.d(TAG, "### 111");
+						Log.d(TAG, "### playAudioBufIndex " + playAudioBufIndex + " indexForGet "  + indexForGet);
+						UdtTools.amrDecoder(playAudioBuf, playAudioBufIndex, pcmArr, 0, Command.CHANEL);
+						for(byte b:pcmArr) {
+							System.out.println(b);
+						}
+						playAudioBufIndex = 0;
+						//Log.d(TAG, "### 222");
+						//m_out_trk.write(pcmArr, 0, pcmBufferLength);
+					}
+					//Log.d(TAG, "### playAudioBufIndex " + playAudioBufIndex + " indexForGet "  + indexForGet);
+					playAudioBuf[playAudioBufIndex] = b0;
+					indexForGet = (indexForGet + 1)%recvAudioBufLen;  
+				}
+			} while(!stopPlay);
+			if (m_out_trk != null) {
+				m_out_trk.stop();
+				m_out_trk.release();
+				m_out_trk = null;
+			}
+			UdtTools.exitAmrDecoder();
+			if(BuildConfig.DEBUG && DEBUG) {
+				Log.d(TAG, "### audio amr decoder exit.");
+			}
+			
+		}
 		
 	}
 }
